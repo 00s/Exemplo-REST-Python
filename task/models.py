c95ed563699fa4d5c207da5ca74d60c5f6907f11
Adolfo Melo <adf.melo@gmail.com>
2015-12-17 09:56:05 -0300

    Estrutura django + app Task

---
+++ task/models.py
@@ -0,0 +1,8 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
